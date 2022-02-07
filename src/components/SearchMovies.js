81e15e52af47a9d073681f8cf023099f8dcdfce6
Charlie Aleck (they/them) <aleckcharlie@gmail.com>
2022-02-07 14:28:27 -0600

    added useState with notes

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SearchMovies() {
+  // states - input query, movies
+  const [query, setQuery] = useState("");
+  //useState('') initial value is an empty string
+  //useState is a function that will update state. setQuery is
+  // a function that will update query.
+  // This is a form of array destructuring
   const searchMovies = async (e) => {
     e.preventDefault();
-    console.log("submitting");
-
-    const query = "Jurassic Park";
 
     const url = `https://api.themoviedb.org/3/movie/550?api_key=7614f64dc9adcab1b7193a3617d9f951&language=en-US&query=${query}&page=1&include_adult=false`;
 
@@ -28,6 +31,8 @@ export default function SearchMovies() {
         type="text"
         name="query"
         placeholder="i.e. Jurassic Park"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
       />
       <button className="button" type="submit">
         Search
