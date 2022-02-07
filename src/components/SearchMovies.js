48b4f1a3f135ba0e479aa6899290ed6368dcc4b2
Charlie Aleck (they/them) <aleckcharlie@gmail.com>
2022-02-07 13:38:37 -0600

    added api key

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -1,8 +1,22 @@
 import React from "react";
 
 export default function SearchMovies() {
+  const searchMovies = async (e) => {
+    e.preventDefault();
+    console.log("submitting");
+
+    const query = "Jurassic Park";
+
+    const url = `https://api.themoviedb.org/3/movie/550?api_key=7614f64dc9adcab1b7193a3617d9f951&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    console.log(data);
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={searchMovies}>
       <label htmlFor="query" className="label">
         Movie Name
       </label>
